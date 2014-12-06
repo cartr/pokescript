8ef32b2f1f648dad2f33f0e5c5e3d796b259ba71
Carter Sande <carter.sande@duodecima.technology>
2014-12-06 09:31:06 -0800

    First pass at the Pokemon list

--- pokeapi.js
+++ pokeapi.js
@@ -21,9 +21,15 @@ function setVarLenText(addr,maxlen,text){
 		gameboy.memory[addrToWrite] = 0x50;
 	}
 }
-/*
-User-callable commands for PokeAPI-js
-*/
+function write16bit(num,addr) {
+	gameboy.memory[addr] = num >> 8;
+	gameboy.memory[addr+1] = num & 0xFF;
+}
+function read16bit(addr) {
+	return (gameboy.memory[addr] << 16) + gameboy.memory[addr+1];
+}
+
+
 function tapButton(key,done) {
 	var validButtons = ["start","select","a","b","up","down","left","right"];
 	if (validButtons.indexOf(key) === -1) {
@@ -41,6 +47,7 @@ function textOnScreen() { return (translateText(50361,18).trim()+"\n"+translateT
 
 function Player() {}
 var player = new Player();
+player.pokemon=[];
 Object.defineProperty(player,"name", {
 	get: function() { return translateVarLenText(0xD158,7)},
 	set: function(name) { setVarLenText(0xD158,7,name) }
@@ -70,7 +77,7 @@ Object.defineProperty(player,"money", {
 		}
 	}
 });
-Object.defineProperty(player,"idNumber", {
+Object.defineProperty(player,"trainerid", {
 	get: function() {
 		return gameboy.memory[0xD359]*256 + gameboy.memory[0xD35A];
 	},
@@ -79,3 +86,99 @@ Object.defineProperty(player,"idNumber", {
 		gameboy.memory[0xD359] = v >> 8;
 	}
 })
+
+function Move(moveid) {
+	this.moveid=moveid;
+	this.pp=0;
+}
+
+function Pokemon(species) {
+	this.species=species;
+	this.hp=5;
+	this.level=1;
+	this.status=0;
+	this.type1=0;
+	this.type2=0;
+	this.moves=[];
+	this.trainerid=player.trainerid;
+	this.exp=0;
+	this.EVs = {hp:0,attack:0,defense:0,speed:0,special:0};
+	this.atkdefiv=0;
+	this.speedspecialiv=0;
+	this.stats = {hp:1,attack:1,defense:1,speed:1,special:1};
+}
+Pokemon.prototype.serialize = function(address) {
+	gameboy.memory[address] = this.species;
+	write16bit(this.hp,address+1);
+	gameboy.memory[address+3] = this.level;
+	gameboy.memory[address+4] = this.status;
+	gameboy.memory[address+5] = this.type1;
+	gameboy.memory[address+6] = this.type2;
+	for (var i=0; i<4; i++) {
+		if (i<this.moves.length)
+			gameboy.memory[address+8+i]=this.moves[i].moveid;
+		else
+			gameboy.memory[address+8+i]=0;
+	}
+	write16bit(this.trainerid,address+12);
+	gameboy.memory[address+14] = this.exp>>14;
+	write16bit(this.exp&0xFFFF,address+15);
+	write16bit(this.EVs.hp,address+17);
+	write16bit(this.EVs.attack,address+19);
+	write16bit(this.EVs.defense,address+21);
+	write16bit(this.EVs.speed,address+23);
+	write16bit(this.EVs.special,address+25);
+	gameboy.memory[address+27] = this.atkdefiv;
+	gameboy.memory[address+28] = this.speedspecialiv;
+	for (var i=0; i<4; i++) {
+		if (i<this.moves.length)
+			gameboy.memory[address+29+i]=this.moves[i].pp;
+		else
+			gameboy.memory[address+29+i]=0;
+	}
+	gameboy.memory[address+33]=this.level;
+	write16bit(this.stats.hp,address+34);
+	write16bit(this.stats.attack,address+36);
+	write16bit(this.stats.defense,address+38);
+	write16bit(this.stats.speed,address+40);
+	write16bit(this.stats.special,address+42);
+}
+Pokemon.prototype.deserialize = function(address) {
+	this.species = gameboy.memory[address];
+	this.hp = read16bit(address+1);
+	this.level = gameboy.memory[address+3];
+	this.status = gameboy.memory[address+4];
+	this.type1 = gameboy.memory[address+5];
+	this.type2 = gameboy.memory[address+6];
+	this.moves=[];
+	for (var i=0; i<4; i++) {
+		if (gameboy.memory[address+8+i] != 0)
+			moves[i]=new Move(gameboy.memory[address+8+i]);
+	}
+	this.trainerid = read16bit(address+12);
+	this.exp = gameboy.memory[address+14] << 14;
+	this.exp += read16bit(address+15);
+	this.EVs.hp = read16bit(address+17);
+	this.EVs.attack = read16bit(address+19);
+	this.EVs.defense = read16bit(address+21);
+	this.EVs.speed = read16bit(address+23);
+	this.EVs.special = raed16bit(address+25);
+	this.atkdefiv = gameboy.memory[address+27];
+	this.speedspecialiv = gameboy.memory[address+28];
+	for (var i=0; i<moves.length; i++) {
+			moves[i].pp=gameboy.memory[address+29+i];
+	}
+	this.level=gameboy.memory[address+33];
+	this.stats.hp=read16bit(address+34);
+	this.stats.attack=read16bit(address+36);
+	this.stats.defense=read16bit(address+38);
+	this.stats.speed=read16bit(address+40);
+	this.stats.special=read16bit(address+42);
+}
+function readPlayerPokemon() {
+	player.pokemon = [];
+	for (var i=0; i<gameboy.memory[0xD163]; i++) {
+		player.pokemon[i]=new Pokemon(gameboy.memory[0xD164+i]);
+		player.pokemon[i].deserialize(0xD16B+44*i);
+	}
+}
