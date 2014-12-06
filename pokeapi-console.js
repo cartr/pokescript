dd0d4c0ac7e7cb6d70eaa8ae97db7280413231cd
Carter Sande <carter.sande@duodecima.technology>
2014-12-06 04:51:46 -0800

    Better command-line feedback

--- pokeapi-console.js
+++ pokeapi-console.js
@@ -2,9 +2,17 @@ function executeCommand() {
 	var cmd = document.getElementById('command').value;
 	document.getElementById('command').value = "";
 	document.getElementById('history').innerHTML += "&gt; "+cmd+"<br>";
-	var result = eval(cmd);
-	if (result !== undefined) {
-		document.getElementById('history').innerHTML += repr(result)+"<br>";
+	try {
+		var result = eval(cmd);
+		if (result !== undefined) {
+			if (typeof result == "number") {
+				document.getElementById('history').innerHTML += result+"<br>";
+			} else {
+				document.getElementById('history').innerHTML += repr(result)+"<br>";
+			}
+		}
+	} catch (e) {
+		document.getElementById('history').innerHTML += e+"<br>";
 	}
 	document.getElementById('history').scrollTop = document.getElementById('history').scrollHeight;
 }
