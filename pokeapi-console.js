4e72dd5e89d7be83b2b9c2b5ec1a54721c6366a3
Carter Sande <carter.sande@duodecima.technology>
2014-12-06 04:14:54 -0800

    Some basic styling

---
+++ pokeapi-console.js
@@ -0,0 +1,10 @@
+function executeCommand() {
+	var cmd = document.getElementById('command').value;
+	document.getElementById('command').value = "";
+	document.getElementById('history').innerHTML += "&gt; "+cmd+"<br>";
+	var result = eval(cmd);
+	if (result !== undefined) {
+		document.getElementById('history').innerHTML += result+"<br>";
+	}
+	document.getElementById('history').scrollTop = document.getElementById('history').scrollHeight;
+}
