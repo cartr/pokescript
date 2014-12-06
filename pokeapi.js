47bc440843044e274f93404b91371b1eacff866f
Carter Sande <carter.sande@duodecima.technology>
2014-12-06 02:16:58 -0800

    Maybe use a queue of keypress events?

--- pokeapi.js
+++ pokeapi.js
@@ -1,8 +1,17 @@
+var keyQueue = [];
+setInterval( function() {
+	var keyFunc = keyQueue.shift();
+	if (keyFunc !== undefined)
+		keyFunc[0](keyFunc[1])
+},100);
 /*
 User-callable commands for PokeAPI-js
 */
-
-function pushButton(key) {
-	GameBoyKeyDown(key);
-	GameBoyKeyUp(key);
+function tapButton(key,done) {
+	keyQueue.push([GameBoyKeyDown,key]);
+	keyQueue.push([function(key) {
+		GameBoyKeyUp(key); 
+		if (done !== undefined)
+			done()
+	}, key]);
 }
