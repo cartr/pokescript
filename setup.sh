257c212cd981efc2678a8ccdc68f45f53fae897e
Carter Sande <carter.sande@duodecima.technology>
2016-01-26 19:40:06 -0800

    Add a precompiled gh-pages version

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-sudo apt-get -y install make gcc bison git python python-setuptools python-pip python-dev
-cd rom-build/rgbds
-sudo make install
-cd ../../pokered
-sudo pip install -r extras/requirements.txt
-make red
-cd ../rom-build
-python build-base64-rom.py
